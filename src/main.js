a1bf71e0a7bb3bc7c2844a4c8bb6a0166d27af6f
Cat-Denton <willwdenton@gmail.com>
2021-03-26 23:44:06 -0700

    add submit and makeApiCall functions to main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeRateService from './exchange-rate-service';
 
+async function makeApiCall(currency) {
+  const response = await ExchangeRateService.getRate(currency);
+}
 
+$("#currency-choice").submit(function() {
+
+});
