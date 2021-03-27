ebcb45df4566d225a23d5b12dcf603ead0b0a97c
Cat-Denton <willwdenton@gmail.com>
2021-03-27 00:00:09 -0700

    add makeApiCall and fillDropDown functions

--- src/main.js
+++ src/main.js
@@ -4,9 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRateService from './exchange-rate-service';
 
+function fillDropDown(response) {
+  let rateKeys = Object.keys(response.conversion_rates);
+  rateKeys.forEach(element => $("#currency").append(`<option>${element}</option>`));
+}
+
 async function makeApiCall(currency) {
   const response = await ExchangeRateService.getRate(currency);
+  fillDropDown(response);
 }
+
+makeApiCall("USD")
 
 $("#currency-choice").submit(function() {
 
