b890e810fcce4729a0b5d94de598e15043dca155
Cat-Denton <willwdenton@gmail.com>
2021-04-06 14:56:52 -0700

    clean up lint warnings

--- src/exchange-rate-service.js
+++ src/exchange-rate-service.js
@@ -3,6 +3,7 @@ export default class ExchangeRateService {
   static getRate(currency) {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
       .then(function(response) {
+        debugger
         if (!response.ok) {
           throw Error(response.statusText);
         }
